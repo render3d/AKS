1ee271d81a4ec2930b562efba5c94def437c8760
Vincent Renders <vince@render3d.tech>
2021-06-25 11:28:45 +0100

    Testing date & time in log file filename

--- test/HPCchronoTest.cpp
+++ test/HPCchronoTest.cpp
@@ -2,21 +2,36 @@
 // cp test/chronoTest.cpp test/HPCchronoTest.cpp
 
 #include <iostream>
+#include <iomanip>
+#include <ctime>
 #include <chrono>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <typeinfo>
+#include <string>
 
 template <typename T> std::string type_name();
 
+std::string getTime() {
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%d-%m-%Y-%H-%M-%S");
+    auto time = oss.str();
+
+    std::cout << time << std::endl;
+
+    return time;
+}
+
 int main() {
-    auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    std::cout << time;
+    getTime();
 
     auto now = std::chrono::high_resolution_clock::now();
     // auto now = std::chrono::steady_clock::now();
-    std::cout << "\nStart\n";
+    std::cout << "Start\n";
 
     //do stuff here
     unsigned int microsecond = 1000000;
