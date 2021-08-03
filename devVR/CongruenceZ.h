34f79c5cf4e65dc917e52ba9b898bfc2a34b9b12
Vincent Renders <vince@render3d.tech>
2021-08-03 10:41:31 +0100

    Renaming files to fix technicalities

---
+++ devVR/CongruenceZ.h
@@ -0,0 +1,11 @@
+
+#include "NTL/ZZ_p.h"
+#include "NTL/ZZ_pX.h"
+NTL_CLIENT
+
+int CongruenceZ(const long& a, const ZZ& n, const ZZ& r){
+    // congruence test of polynomials in large integer form
+
+    //
+
+}
