ff6cc04f1864111b5d237c1686f2eaf37ecfad93
Vincent Renders <vince@render3d.tech>
2021-08-09 11:29:07 +0100

    Added basic compile instructions to HPC programs in comment

--- devVR/HPCLenstraZ.cu
+++ devVR/HPCLenstraZ.cu
@@ -3,6 +3,8 @@
         https://researchportal.bath.ac.uk/en/publications/the-analysis-and-implementation-of-the-aks-algorithm-and-its-impr
     Compile with:
         $ g++ -g -O2 -std=c++11 -pthread -march=native dir/foo.cpp -o dir/foo.out -lntl -lgmp -lm
+        $ nvcc -o HPCLenstraZnx.out HPCLenstraZnx.cu
+        $ nvcc HPCLenstraZnx.cu -o HPCLenstraZnx.out
 */
 
 #include <math.h> // standard libraries
