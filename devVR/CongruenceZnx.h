08cc767adcaa8514bafa58aec9ce3ddfffae574f
Vincent Renders <vince@render3d.tech>
2021-08-17 13:53:08 +0100

    Corrected logic errors in for loop.

--- devVR/CongruenceZnx.h
+++ devVR/CongruenceZnx.h
@@ -1,31 +1,32 @@
 
 #include "NTL/ZZ_p.h"
 #include "NTL/ZZ_pX.h"
-#include <NTL/BasicThreadPool.h>
+// #include <NTL/BasicThreadPool.h>
 NTL_CLIENT
 
 unsigned int ncores = std::thread::hardware_concurrency(); // machine cores - may return 0 when not able to detect
-const auto SetNumThreads(ncores); // number of threads - should correspond to the number of available cores on your machine
+// const auto SetNumThreads(ncores); // number of threads - should correspond to the number of available cores on your machine
 
 long CongruenceZnx(const ZZ& n, const ZZ& r, const ZZ& r2, const long& a){
     // congruence test of polynomials in regular form
 
     ZZ_p::init(n);                      // initialise mod n
-    ZZ_pX b = ZZ_pX(to_long(r), 1) - 1; // b = x^r - 1;
-    ZZ_pX e = ZZ_pX(1, 1);              // e = x
-    ZZ_pX d = PowerMod(e, n, b);        // d = x^n mod b, n
+
+    ZZ_pX b = ZZ_pX(to_long(r), 1) - 1; // b = x^r - 1 (mod n);
+    ZZ_pX e = ZZ_pX(1, 1);              // e = x (mod n)
+    ZZ_pX d = PowerMod(e, n, b);        // d = x^n (mod b, n)
 
     // NTL_EXEC_RANGE(a,first,last)
 
         // for(long j = first; j <= last; ++j){
         for(long j = 1; j <= a; ++j){
 
-            ZZ_pX c = ZZ_pX(1, 1) - a ;         // c = x - a;
-            ZZ_pX f = PowerMod(c, n, b);        // f =(x - a)^n (mod b, n) - LHS
-            ZZ_pX g = d - a;                    // g = x^n - a (mod b, n) - RHS
+            ZZ_pX c = ZZ_pX(1, 1) - j ;         // c = x - a (mod n);
+            ZZ_pX f = PowerMod(c, n, b);        // f = (x - a)^n (mod b, n) - LHS
+            ZZ_pX g = d - j;                    // g = x^n - a (mod b, n) - RHS
 
             if(f != g){
-                return(to_long(a)); // n is not prime
+                return(to_long(j)); // n is not prime
             }
         }
 
