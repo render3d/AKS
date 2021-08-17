96d868d1604c62800290f728ce3eab70ddc84a5e
Vincent Renders <vince@render3d.tech>
2021-08-17 21:52:53 +0100

    Implemented perfected powMod function. Types need checking/changing to ZZ*/NTL compatible.

--- devVR/CongruenceZ.h
+++ devVR/CongruenceZ.h
@@ -1,9 +1,27 @@
 
+#include "NTL/ZZ.h"
 #include "NTL/ZZ_p.h"
 #include "NTL/ZZ_pX.h"
 NTL_CLIENT
 
-int CongruenceZ(const ZZ& n, const ZZ& r, const ZZ& r2){
+long powMod(long a, long n, long b) {
+    // calculates a^n in O(log n)
+
+    long ans = 1;            // Initialise answer
+
+    while (n > 0) {
+        if (n % 2 == 1) {   // if (n is odd) then
+            ans = (ans * a) % b;
+        }
+
+        a = (a * a) % b;    // a = a^2 (mod b)
+        n /= 2;             // n = n/2
+    }
+
+    return ans;
+}
+
+int CongruenceZ(const ZZ& n, const ZZ& r, const ZZ& r2) {
     // congruence test of polynomials in large integer form
 
     return 0;
