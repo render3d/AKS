9fbccc56747c816f0d7203486c1895e6629096da
Vincent Renders <vince@render3d.tech>
2021-08-18 15:22:23 +0100

    Added temporary skeleton structure to CongruenceZ from CongruenceZnx. To be ammended.

--- devVR/CongruenceZ.h
+++ devVR/CongruenceZ.h
@@ -93,9 +93,25 @@ ZZX polyMultiply(ZZX f, ZZX g) {
     return polyProduct;
 }
 
-int CongruenceZ(const ZZ& n, const ZZ& r, const ZZ& r2) {
+long CongruenceZ(const ZZ& n, const ZZ& r, const ZZ& r2, const long& a) {
     // congruence test of polynomials in large integer form
 
-    return 0;
+    ZZ_p::init(n);                      // initialise mod n
 
+    ZZ_pX b = ZZ_pX(to_long(r), 1) - 1; // b = x^r - 1 (mod n);
+    ZZ_pX e = ZZ_pX(1, 1);              // e = x (mod n)
+    ZZ_pX d = PowerMod(e, n, b);        // d = x^n (mod b, n)
+
+    for(long j = 1; j <= a; ++j){
+
+        ZZ_pX c = ZZ_pX(1, 1) - j ;         // c = x - a (mod n);
+        ZZ_pX f = PowerMod(c, n, b);        // f = (x - a)^n (mod b, n) - LHS
+        ZZ_pX g = d - j;                    // g = x^n - a (mod b, n) - RHS
+
+        if(f != g){
+            return(to_long(j)); // n is not prime
+        }
+    }
+
+    return(0); // n is prime
 }
