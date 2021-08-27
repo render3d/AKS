42d60c0d2957537540ad6d4c3cfb2e408543c72c
Vincent Renders <vince@render3d.tech>
2021-08-27 14:49:53 +0100

    Add (and test) NTL Thread Pools

--- devVR/CongruenceZnx.h
+++ devVR/CongruenceZnx.h
@@ -1,11 +1,11 @@
 
 #include "NTL/ZZ_p.h"
 #include "NTL/ZZ_pX.h"
-// #include <NTL/BasicThreadPool.h>
+#include <NTL/BasicThreadPool.h>
 NTL_CLIENT
 
 unsigned int ncores = std::thread::hardware_concurrency(); // machine cores - may return 0 when not able to detect
-// const auto SetNumThreads(ncores); // number of threads - should correspond to the number of available cores on your machine
+const auto SetNumThreads(ncores); // number of threads - should correspond to the number of available cores on your machine
 
 long CongruenceZnx(const ZZ& n, const ZZ& r, const ZZ& r2, const long& a){
     // congruence test of polynomials in regular form
@@ -24,23 +24,23 @@ long CongruenceZnx(const ZZ& n, const ZZ& r, const ZZ& r2, const long& a){
     ZZ_pX d = PowerMod(e, n, b);        // d = x^n (mod b, n)
     printf("Done.\n");
 
-    // NTL_EXEC_RANGE(a,first,last)
-
-        // for(long j = first; j <= last; ++j){
     printf("\nCommencing congruence tests:\n\n");
-    for(long j = 1; j <= a; ++j){
-        // printf("a = %ld\n",j);
+    NTL_EXEC_RANGE(a,first,last)
 
-        ZZ_pX c = ZZ_pX(1, 1) - j ;         // c = x - a (mod n);
-        ZZ_pX f = PowerMod(c, n, b);        // f = (x - a)^n (mod b, n) - LHS
-        ZZ_pX g = d - j;                    // g = x^n - a (mod b, n) - RHS
+        for(long j = first; j <= last; ++j){
+        // for(long j = 1; j <= a; ++j){
+            printf("a = %ld\n",j);
 
-        if(f != g){
-            return(j); // n is not prime
+            ZZ_pX c = ZZ_pX(1, 1) - j ;         // c = x - a (mod n);
+            ZZ_pX f = PowerMod(c, n, b);        // f = (x - a)^n (mod b, n) - LHS
+            ZZ_pX g = d - j;                    // g = x^n - a (mod b, n) - RHS
+
+            if(f != g){
+                return(j); // n is not prime
+            }
         }
-    }
 
-    // NTL_EXEC_RANGE_END
+    NTL_EXEC_RANGE_END
 
     return(0); // n is prime
 }
